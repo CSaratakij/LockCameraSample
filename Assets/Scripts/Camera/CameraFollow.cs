e37135d62c9a5792b0d6d1ca60c6956801b9ae10
Chatchai Saratakij <CSaratakij@gmail.com>
2019-12-21 15:09:16 +0700

    Finish example

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    float damp = 0.025f;
+
+    [SerializeField]
+    Transform target;
+
+    Vector3 offset;
+    Vector3 destination;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void LateUpdate()
+    {
+        FollowHandler();
+    }
+
+    void Initialize()
+    {
+        offset = transform.position - target.position;
+    }
+
+    void FollowHandler()
+    {
+        destination = target.position + offset;
+        transform.position = Vector3.Lerp(transform.position, destination, damp);
+    }
+}
+
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5.0f;
+
+    [SerializeField]
+    float gravity = 9.8f;
+
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    float rotateDamp = 0.2f;
+
+    Vector3 inputVector;
+    Vector3 moveDirection;
+    Vector3 lastDirection;
+    Vector3 velocity;
+
+    CharacterController characterController;
+
+    void Awake()
+    {
+        Initialize();
+    }
+
+    void Update()
+    {
+        InputHandler();
+        MovementHandler();
+    }
+
+    void LateUpdate()
+    {
+        FacingHandler();
+    }
+
+    void Initialize()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void InputHandler()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void MovementHandler()
+    {
+        if (moveDirection != Vector3.zero)
+        {
+            lastDirection = moveDirection;
+        }
+
+        moveDirection.x = inputVector.x;
+        moveDirection.z = inputVector.y;
+
+        velocity = moveDirection * moveSpeed;
+        velocity.y -= gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+    }
+
+    void FacingHandler()
+    {
+        Quaternion lookRotation = Quaternion.LookRotation(lastDirection);
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, rotateDamp);
+    }
+}
+
